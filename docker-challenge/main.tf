299873f74806e7f828f7ee9030ea335230fb3fc9
SamSymonds <samsymonds@hotmail.co.uk>
2024-10-29 20:13:53 +0000

    terraform null resource loops

--- docker-challenge/main.tf
+++ docker-challenge/main.tf
@@ -7,4 +7,31 @@ terraform {
  }
 }
 
+provider "docker" {}
+
+resource "docker_image" "simplegoservice" {
+    name        = "simplegoservice"
+}
+
+resource "docker_container" "simplegoservice" {
+    image       = docker_image.simplegoservice
+    name        = var.container_name
+    ports {
+        internal = var.internal_port
+        external = var.external_port
+
+variable "container_name" {
+    default     = "Alta3ResearchWebService"
+}
+
+variable "internal_port" {
+    type     = number
+    default  = 9876
+}
+
+variable "external_port" {
+    type     = number
+    default  = 5432
+}
+
 
